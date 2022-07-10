42971bfdfcd5dd479ef5d3cbf8c34ef9f569bd21
Apanyold <51063161+Apanyold@users.noreply.github.com>
2022-07-10 20:53:07 +0300

    Adding bg for spheres

--- Assets/Scripts/Spells/MagicShpere.cs
+++ Assets/Scripts/Spells/MagicShpere.cs
@@ -10,9 +10,15 @@ public class MagicShpere : MonoBehaviour
     [SerializeField]
     private CircleCollider2D circleCollider;
 
+    [SerializeField]
+    private GameObject radiusIndicator;
+
     private void Awake()
     {
-        circleCollider.radius = magicShpereConfig.pickUpRadius;
+        var radius = magicShpereConfig.pickUpRadius;
+        circleCollider.radius = radius;
+
+        radiusIndicator.transform.localScale = new Vector3(radius*2, radius*2, 1);
     }
 
     public void SetCaster(GameObject caster)
