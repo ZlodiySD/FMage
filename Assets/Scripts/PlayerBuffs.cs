d42843fea1ab3ff1af2426854b746d837a59cf05
Apanyold <51063161+Apanyold@users.noreply.github.com>
2022-07-10 14:22:40 +0300

    fix pref commit

---
+++ Assets/Scripts/PlayerBuffs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class PlayerBuffs
+{
+    public bool FireResistance;
+
+    public void SetBuffState(BuffType buffType, bool isActive)
+    {
+        switch (buffType)
+        {
+            case BuffType.FireResistance:
+                FireResistance = isActive;
+                break;
+        }
+    }
+
+    public bool IsBuffActive(BuffType requiredBuffType)
+    {
+        switch (requiredBuffType)
+        {
+            case BuffType.FireResistance:
+                return FireResistance;
+            default:
+                return false;
+        }
+    }
+}
